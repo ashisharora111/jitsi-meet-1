d6a7dd2aa86cac9a124ce86369a930cd32fb7ac8
ashisharora111 <43462035+ashisharora111@users.noreply.github.com>
2021-03-21 10:11:00 +0530

    Update and rename config.js.example to config.js

---
+++ doc/example-config-files/config.js
@@ -0,0 +1,8 @@
+var config = {
+    hosts: {
+        domain: 'meet.aroraas.people.aws.dev',
+        muc: 'conference.meet.aroraas.people.aws.dev', // FIXME: use XEP-0030
+        bridge: 'jitsi-videobridge.meet.aroraas.people.aws.dev' // FIXME: use XEP-0030
+    },
+    bosh: '//meet.aroraas.people.aws.dev/http-bind' // FIXME: use xep-0156 for that
+};
