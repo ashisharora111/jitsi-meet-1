cb3c2fc7353302c2584e8a3702ea84c9b5e18076
ashisharora111 <43462035+ashisharora111@users.noreply.github.com>
2021-03-21 17:44:40 +0530

    Update config.js

--- doc/example-config-files/config.js
+++ doc/example-config-files/config.js
@@ -1,8 +1,8 @@
 var config = {
     hosts: {
-        domain: 'meet.aroraas.people.aws.dev',
-        muc: 'conference.meet.aroraas.people.aws.dev', // FIXME: use XEP-0030
-        bridge: 'jitsi-videobridge.meet.aroraas.people.aws.dev' // FIXME: use XEP-0030
+        domain: 'jitsi-meet.aroraas.people.aws.dev',
+        muc: 'conference.jitsi-meet.aroraas.people.aws.dev', // FIXME: use XEP-0030
+        bridge: 'jitsi-videobridge.jitsi-meet.aroraas.people.aws.dev' // FIXME: use XEP-0030
     },
-    bosh: '//meet.aroraas.people.aws.dev/http-bind' // FIXME: use xep-0156 for that
+    bosh: '//jitsi-meet.aroraas.people.aws.dev/http-bind' // FIXME: use xep-0156 for that
 };
